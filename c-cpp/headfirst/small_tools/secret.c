10bed9623e6240528464fe8e8202fcc2c72274c9
250lth <gang250lth@gmail.com>
2017-04-22 19:36:03 +0800

    use stdout and stderr to crack secret

---
+++ c-cpp/headfirst/small_tools/secret.c
@@ -0,0 +1,18 @@
+//
+// Created by lth on 17-4-22.
+//
+
+#include <stdio.h>
+
+int main3c() {
+    char word[10];
+    int i = 0;
+    while (scanf("%9s", word) == 1) {
+        i = i + 1;
+        if (i % 2)
+            fprintf(stdout, "%s\n", word);
+        else
+            fprintf(stderr, "%s\n", word);
+    }
+    return 0;
+}
