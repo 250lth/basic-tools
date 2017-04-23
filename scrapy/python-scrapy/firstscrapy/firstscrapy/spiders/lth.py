00c3024513300a5105a6477aaea7ab7943c9a2ec
250lth <gang250lth@gmail.com>
2017-04-23 09:49:20 +0800

    rewrite start_requests() to use other start_urls

--- scrapy/python-scrapy/firstscrapy/firstscrapy/spiders/lth.py
+++ scrapy/python-scrapy/firstscrapy/firstscrapy/spiders/lth.py
@@ -9,6 +9,13 @@ class LthSpider(scrapy.Spider):
     start_urls = ['http://slide.news.sina.com.cn/s/slide_1_2841_123175.html',
                   'http://slide.mil.news.sina.com.cn/k/slide_8_260_50032.html/d/#p=1',
                   'http://news.sina.com.cn/china/xlxw/2017-04-22/doc-ifyepsch2529468.shtml']
+    urls2 = ["http://www.jd.com",
+             "http://sina.com.cn",
+             "http://yum.iqianyue.com"]
+
+    def start_requests(self):
+        for url in self.urls2:
+            yield self.make_requests_from_url(url)
 
     def parse(self, response):
         item = FirstscrapyItem()
