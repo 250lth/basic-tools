3b60d2890788eab5e53af32c65a792f1e0cad8bd
250lth <gang250lth@gmail.com>
2017-04-23 14:49:47 +0800

    create .h to put defs in it

--- c-cpp/headfirst/multiple_sources/data_type_demo.c
+++ c-cpp/headfirst/multiple_sources/data_type_demo.c
@@ -4,25 +4,25 @@
 
 #include <stdio.h>
 
-float total = 0.0;
-short count = 0;
-short tax_percent = 6;
+float total1 = 0.0;
+short count1 = 0;
+short tax_percent1 = 6;
 
-float add_with_tax(float f){
-    float tax_rate = 1 + tax_percent / 100.0;
-    total = total + (f * tax_rate);
-    count = count + 1;
-    return total;
+float add_with_tax1(float f){
+    float tax_rate1 = 1 + tax_percent1 / 100.0;
+    total1 = total1 + (f * tax_rate1);
+    count1 = count1 + 1;
+    return total1;
 }
 
-int main() {
+int main4a() {
     float val;
     printf("Price of item: ");
     while (scanf("%f", &val) == 1) {
-        printf("Total so far: %.2f\n", add_with_tax(val));
+        printf("Total so far: %.2f\n", add_with_tax1(val));
         printf("Price of item: ");
     }
-    printf("\nFinal total: %.2f\n", total);
-    printf("Number of items: %hi\n", count);
+    printf("\nFinal total: %.2f\n", total1);
+    printf("Number of items: %hi\n", count1);
     return 0;
 }
---
+++ c-cpp/headfirst/multiple_sources/totaller.h
@@ -0,0 +1,5 @@
+//
+// Created by lth on 17-4-23.
+//
+
+float add_with_tax(float f);
