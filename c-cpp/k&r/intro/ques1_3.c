7648cdc2547e0a4f19e28367ec4c7d26da95c42f
250lth <gang250lth@gmail.com>
2017-04-27 15:32:13 +0800

    c num type and calc expression

---
+++ c-cpp/k&r/intro/ques1_3.c
@@ -0,0 +1,28 @@
+//
+// Created by lth on 17-4-27.
+//
+
+#include <stdio.h>
+
+/**
+ * Print Fahrenheit-Celsius table
+ * @return
+ */
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahr   Celsius\n");
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f    %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
