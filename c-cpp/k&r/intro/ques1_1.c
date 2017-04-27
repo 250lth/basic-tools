5b29320c6cf33302d060c0810f8bbad0ade33285
250lth <gang250lth@gmail.com>
2017-04-27 14:52:35 +0800

    start k&r c

---
+++ c-cpp/k&r/intro/ques1_1.c
@@ -0,0 +1,10 @@
+//
+// Created by lth on 17-4-27.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world\n");
+    printf("Hello, world");
+}
---
+++ c-cpp/k&r/intro/ques1_2.c
@@ -0,0 +1,11 @@
+//
+// Created by lth on 17-4-27.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world\y");   //a warning
+    printf("Hello, world\7");
+    printf("Hello, world\?");
+}
