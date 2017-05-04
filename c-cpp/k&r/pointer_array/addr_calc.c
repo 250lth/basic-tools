44acf1ca2dec323084136851b0542e53a65806d0
250lth <gang250lth@gmail.com>
2017-05-04 15:45:31 +0800

    calculation of address in c pointer

---
+++ c-cpp/k&r/pointer_array/addr_calc.c
@@ -0,0 +1,21 @@
+//
+// Created by lth on 17-5-4.
+//
+
+#define ALLOCSIZE 100
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n) {
+    if (allocbuf + ALLOCSIZE - allocp >= n) {
+        allocp += n;
+        return allocp - n;
+    } else
+        return 0;
+}
+
+void afree(char *p) {
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
