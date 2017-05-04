4524133a486426bf2d9466f157d147e0f3d5d2e8
250lth <gang250lth@gmail.com>
2017-05-04 21:05:23 +0800

    function pointer sample qsort

---
+++ c-cpp/k&r/pointer_array/func_pointer/rw.c
@@ -0,0 +1,54 @@
+//
+// Created by lth on 17-5-4.
+//
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 1000
+#define ALLOCSIZE 100
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+int getlines(char *s, int lim);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines) {
+    int len, nlines;
+    char *p, line[MAXLINE];
+    nlines = 0;
+    while ((len = getlines(line, MAXLINE)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else {
+            line[len - 1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+void writelines(char *lineptr[], int nlines) {
+    while (nlines-- > 0)
+        printf("%s\n", *lineptr++);
+}
+
+int getlines(char *s, int lim) {
+    int c;
+    char *t = s;
+
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        *s++ = c;
+    if (c == '\n')
+        *s++ = c;
+    *s = '\0';
+    return s - t;
+}
+
+char *alloc(int n) {
+    if (allocbuf + ALLOCSIZE - allocp >= n) {
+        allocp += n;
+        return allocp - n;
+    } else
+        return 0;
+}
