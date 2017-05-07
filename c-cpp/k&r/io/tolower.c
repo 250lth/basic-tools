59a6541a1cd820a352e7e7bb8cb782bd9e7c21f8
250lth <gang250lth@gmail.com>
2017-05-07 19:52:45 +0800

    sample of ctype.h -- tolower()

---
+++ c-cpp/k&r/io/tolower.c
@@ -0,0 +1,14 @@
+//
+// Created by lth on 17-5-7.
+//
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    int c;
+
+    while ((c = getchar()) != EOF)
+        putchar(tolower(c));
+    return 0;
+}
