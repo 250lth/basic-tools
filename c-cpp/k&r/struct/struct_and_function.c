1983cf00a4437630387d6155718ba1861b4cc1dd
250lth <gang250lth@gmail.com>
2017-05-07 11:11:15 +0800

    intro of c struct

---
+++ c-cpp/k&r/struct/struct_and_function.c
@@ -0,0 +1,46 @@
+//
+// Created by lth on 17-5-7.
+//
+
+struct point {
+    int x;
+    int y;
+};
+
+struct rect {
+    struct point pt1;
+    struct point pt2;
+};
+
+struct point makepoint(int x, int y) {
+    struct point temp;
+
+    temp.x = x;
+    temp.y = y;
+    return temp;
+}
+
+struct point addpoint(struct point p1, struct point p2) {
+    p1.x += p2.x;
+    p1.y += p2.y;
+    return p1;
+}
+
+int ptinrect(struct point p, struct rect r) {
+    return p.x >= r.pt1.x && p.x < r.pt2.x &&
+                             p.y >= r.pt1.y && p.y < r.pt2.y;
+}
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+struct rect canorect(struct rect r) {
+    struct rect temp;
+
+    temp.pt1.x = min(r.pt1.x, r.pt2.x);
+    temp.pt1.y = min(r.pt1.y, r.pt2.y);
+    temp.pt2.x = max(r.pt1.x, r.pt2.x);
+    temp.pt2.y = max(r.pt1.y, r.pt2.y);
+
+    return temp;
+}
