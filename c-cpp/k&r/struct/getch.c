0edd811a940a996dc7e73fd5ecba6ba44cc42e95
250lth <gang250lth@gmail.com>
2017-05-07 13:28:55 +0800

    define a struct to record data for kw counter

---
+++ c-cpp/k&r/struct/getch.c
@@ -0,0 +1,21 @@
+//
+// Created by lth on 17-5-3.
+//
+
+#include <stdio.h>
+
+#define BUFSIZE 100
+
+static char buf[BUFSIZE];
+static int bufp = 0;
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
