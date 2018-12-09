f7e395698364a6c159adbc84720994ff2e246105
250lth <gang250lth@gmail.com>
2018-12-09 22:31:55 +0800

    sample of dict and set

---
+++ python/fluent/dick-set/strkeydict0.py
@@ -0,0 +1,15 @@
+class StrKeyDict0(dict):
+
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError(key)
+        return self[str(key)]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        return key in self.keys() or str(key) in self.keys()
