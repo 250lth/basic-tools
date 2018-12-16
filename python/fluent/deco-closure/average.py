3fa46f909423d578d57f88a506f6c51c0d37cdff
250lth <gang250lth@gmail.com>
2018-12-16 22:41:28 +0800

    sample of decorator and closure

---
+++ python/fluent/deco-closure/average.py
@@ -0,0 +1,11 @@
+def make_averager():
+    count = 0
+    total = 0
+
+    def averager(new_value):
+        nonlocal count, total
+        count += 1
+        total += new_value
+        return total / len(series)
+
+    return averager
---
+++ python/fluent/deco-closure/average_oo.py
@@ -0,0 +1,9 @@
+class Averager():
+
+    def __init__(self):
+        self.series = []
+
+    def __call__(self, new_value):
+        self.series.append(new_value)
+        total = sum(self.series)
+        return total / len(self.series)
